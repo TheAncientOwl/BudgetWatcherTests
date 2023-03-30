686ffcf2f59cc8f7412ae2aeb75b3a0cd8ea1bf7
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-03-30 17:05:21 +0300

    first commit

---
+++ Database/Schemas/IncomeTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BudgetDjinni.Database.Schemas;
+
+namespace BudgetDjinniTests.Database.Schemas
+{
+    [TestClass]
+    public class IncomeTests
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            BudgetDjinni.Database.Manager.Instance.OpenOrCreateDatabase();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            BudgetDjinni.Database.Manager.Instance.CloseDatabase(); 
+        }
+
+        [TestMethod]
+        public void LoadIncomeFromId()
+        {
+            Income income1 = new Income("Salariu", 4000);
+            income1.Save();
+
+            Income income2 = new Income(income1.Id);
+
+            Assert.AreEqual(income1.Id, income2.Id);
+            Assert.AreEqual(income1.Name, income2.Name);
+            Assert.AreEqual(income1.Value, income2.Value);
+        }
+
+        [TestMethod]
+        public void UpdateIncome()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteIncome()
+        {
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BudgetDjinniTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudgetDjinniTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("45fcc498-b2fc-4537-a53e-1b889eda3146")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
